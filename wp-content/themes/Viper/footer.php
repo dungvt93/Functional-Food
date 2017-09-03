2d02aeb4b6f3eee24e66c7ab56b7c384ab4d7536
dungvt93 <tiendung_px@bka.vn>
2017-09-03 09:48:01 +0900

    resolv conflict and change footer

--- wp-content/themes/Viper/footer.php
+++ wp-content/themes/Viper/footer.php
@@ -27,7 +27,7 @@
 <div class="site-info">
 		<div class="fcred">
 		Copyright &copy; <?php echo date('Y');?> <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> - <?php bloginfo('description'); ?>.<br />
-<?php fflink(); ?> | <a href="http://fabthemes.com/<?php echo FT_scope::tool()->themeName ?>/" ><?php echo FT_scope::tool()->themeName ?> WordPress Theme</a>
+<?php fflink(); ?> | Advice for health
 		</div>		
 </div><!-- .site-info -->	
 </footer><!-- #colophon .site-footer -->
