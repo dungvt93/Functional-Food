0a71af45c22254203e0fba6c5991f86ef902b20f
dungvt93 <tiendung_px@bka.vn>
2017-09-16 21:38:33 +0900

    chat room

--- wp-content/themes/Viper/content-products.php
+++ wp-content/themes/Viper/content-products.php
@@ -33,7 +33,7 @@
 
 
 	</div>
-    <embed wmode="transparent" src="https://www.xatech.com/web_gear/chat/chat.swf" quality="high" width="540" height="405" name="chat" FlashVars="id=219562388" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="https://xat.com/update_flash.php" /><br><small><a target="_BLANK" href="https://xat.com/web_gear/?cb">Get your own Chat Box!</a> <a target="_BLANK" href="https://xat.com/web_gear/chat/go_large.php?id=219562388">Go Large!</a></small><br>
+
 	<div class="product-single-bottom">
 	
 	<div class="entry-content">
