80f2e60b260b817b5eb26df0c3cf2230f7c5168e
root <root@ip-172-31-19-77.ap-northeast-1.compute.internal>
2017-09-25 13:15:49 +0000

    xoa plugin image url featured

--- wp-content/plugins/featured-image-from-url/includes/html/js/image.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-    setTimeout(function () {
-        jQuery('div.flex-viewport').each(function (index) {
-            jQuery(this).css('height', '');
-        });
-    }, 500);
-});
