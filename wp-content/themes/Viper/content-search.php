37d83213ca718cb7b085c247f031ea148169daed
dungvt93 <tiendung_px@bka.vn>
2017-09-26 21:53:43 +0900

    template blog

--- wp-content/themes/Viper/content-search.php
+++ wp-content/themes/Viper/content-search.php
@@ -9,7 +9,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'web2feel' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
