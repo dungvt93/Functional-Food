e37910bf856be4cb4501b2f0e79fc09718b4887d
dungvt93 <tiendung_px@bka.vn>
2017-10-09 17:18:08 +0900

    add page facebook

--- wp-content/themes/Viper/homepage.php
+++ wp-content/themes/Viper/homepage.php
@@ -55,8 +55,8 @@ get_header(); ?>
 		</div>
 		
 		<div id="banner-block">
-			<div class="head-banner">	<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbestfoodhealth%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=561129747373393" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>	</div>
-			
+<!--			<div class="head-banner">	--><?php //echo ft_of_get_option('fabthemes_off_banner');	?><!--	</div>-->
+			<div class="head-banner"><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbestfoodhealth%2F&tabs&width=340&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=561129747373393" width="340" height="214" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></div>
 			
 		</div>
 		
